dbca3b6aa040656383981ba70e42abc9c4931721
Mike Jackson <mike.jackson@bluequartz.net>
2011-01-26 14:38:27 -0500

    Rmeoving some files

--- COde/emmpm/mt/SourceList.cmake
+++
@@ -1,8 +0,0 @@
-set (EMMPM_mt_SRCS
-    ${EMMPMLib_SOURCE_DIR}/mt/mt19937ar.c
-)
-
-set (EMMPM_mt_HDRS
-    ${EMMPMLib_SOURCE_DIR}/mt/mt19937ar.h
-)
-cmp_IDE_SOURCE_PROPERTIES( "emmpm/mt" "${EMMPM_mt_HDRS}" "${EMMPM_mt_SRCS}" "${CMP_INSTALL_FILES}")
