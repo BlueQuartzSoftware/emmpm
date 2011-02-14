53c3fea8e00bd7a6adeae08c7aefaeeee5c9a2c5
Mike Jackson <mike.jackson@bluequartz.net>
2011-02-14 13:51:29 -0500

    Fixing installation of missing header files when doing an install

--- Code/emmpm/common/SourceList.cmake
+++ Code/emmpm/common/SourceList.cmake
@@ -1,5 +1,4 @@
 set (EMMPM_Common_SRCS
-#    ${EMMPMLib_SOURCE_DIR}/common/allocate.c
     ${EMMPMLib_SOURCE_DIR}/common/EMTime.c
     ${EMMPMLib_SOURCE_DIR}/common/entropy.c
 )
@@ -7,14 +6,13 @@ set (EMMPM_Common_SRCS
 set (EMMPM_Common_HDRS
     ${EMMPMLib_SOURCE_DIR}/common/MSVCDefines.h
     ${EMMPMLib_SOURCE_DIR}/common/DLLExport.h
- #   ${EMMPMLib_SOURCE_DIR}/common/allocate.h
     ${EMMPMLib_SOURCE_DIR}/common/EMTime.h
     ${EMMPMLib_SOURCE_DIR}/common/entropy.h
+    ${EMMPMLib_SOURCE_DIR}/common/random.h
 )
 
 if (NOT EMMPM_USE_MT_RNG)
-    set (EMMPM_Common_SRCS ${EMMPM_Common_SRCS} ${EMMPMLib_SOURCE_DIR}/common/random.c )
-    set (EMMPM_Common_HDRS ${EMMPM_Common_HDRS} ${EMMPMLib_SOURCE_DIR}/common/random.h )
+set (EMMPM_Common_SRCS ${EMMPM_Common_SRCS}     ${EMMPMLib_SOURCE_DIR}/common/random.c )
 endif()
 
 cmp_IDE_SOURCE_PROPERTIES( "emmpm/common" "${EMMPM_Common_HDRS}" "${EMMPM_Common_SRCS}" "${CMP_INSTALL_FILES}")
