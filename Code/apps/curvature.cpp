3cb27f2f6a3e7486f6358595c17d14e1b0c06ae1
Michael Jackson <mike.jackson@bluequartz.net>
2011-03-29 15:15:43 -0400

    Removing CMP Project as it will become a submodule

--- Code/apps/curvature.cpp
+++ Code/apps/curvature.cpp
@@ -28,6 +28,8 @@
  * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 
+#error This file is no longer used.
+
 #include <string.h>
 #include <stdlib.h>
 
