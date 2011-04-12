cbcbdb4ffce7ab70d1ebf4f7d0a63bd727042bf6
Mike Jackson <mike.jackson@bluequartz.net>
2011-04-12 16:48:58 -0400

    Fixing license and Copyright text

--- Code/emmpm/common/SourceList.cmake
+++ Code/emmpm/common/SourceList.cmake
@@ -1,3 +1,9 @@
+#--////////////////////////////////////////////////////////////////////////////
+#-- Copyright (c) 2009, Michael A. Jackson. BlueQuartz Software
+#-- All rights reserved.
+#-- BSD License: http://www.opensource.org/licenses/bsd-license.html
+#-- This code was partly written under US Air Force Contract FA8650-07-D-5800
+#--////////////////////////////////////////////////////////////////////////////
 set (EMMPM_Common_SRCS
     ${EMMPMLib_SOURCE_DIR}/common/EMTime.c
     ${EMMPMLib_SOURCE_DIR}/common/entropy.c
